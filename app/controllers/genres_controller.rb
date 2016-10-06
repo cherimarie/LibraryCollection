4c5297876514e12f4811880d6484a9879f78e01f
Cheri Allen <Gargantua@Dimme-Kur.local>
2016-10-05 18:42:25 -0700

    Fix genre no pagination bug; standardize helper methods

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -7,9 +7,11 @@ class GenresController < ApplicationController
   helper_method :sort_column, :sort_direction
 
   def index
+    @genres = Genre.all.order(sort_column + " " + sort_direction).paginate(:page => params[:page], :per_page => 50)
     respond_to do |format|
-      format.html {@genres = Genre.all.order(sort_column + " " + sort_direction).paginate(:page => params[:page], :per_page => 50) }
-      format.json {render json: Genre.all.as_json}
+      format.html {}
+      format.js {}
+      format.json {render json: @genres.as_json}
     end
   end
 
@@ -80,12 +82,16 @@ private
     @genre = Genre.find(params[:id])
   end
 
-  def sort_column(default = "name")
-    params[:sort] ? params[:sort] : default
+  def sort_column(column_name = nil)
+    unless column_name
+      params[:sort] ? params[:sort] : "name"
+    else
+      column_name
+    end
   end
 
-  def sort_direction
-    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+  def sort_direction(default = "asc")
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : default
   end
 
   def genre_params
